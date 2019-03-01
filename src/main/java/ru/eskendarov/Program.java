9aa6280f6f0962e86271aff817e35ee1a25c465c
Eskendarov <envereskendarov@gmail.com>
2019-03-01 19:05:43 +0300

    init commit

---
+++ src/main/java/ru/eskendarov/Program.java
@@ -0,0 +1,11 @@
+package ru.eskendarov;
+
+/**
+ * @author Enver Eskendarov
+ */
+public class Program {
+
+  public static void main(final String[] args) {
+
+  }
+}
---
+++ src/test/java/ru/eskendarov/ProgramTest.java
@@ -0,0 +1,18 @@
+package ru.eskendarov;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for Program.
+ *
+ * @author Enver Eskendarov
+ */
+public class ProgramTest {
+
+  @Test
+  public void testFirst() {
+    assertTrue(true);
+  }
+}
